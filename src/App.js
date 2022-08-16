4e4434ccfc7289d2eec96f1edf39e2ed866833fa
Valeh <vlangeroudi@logic.tv>
2022-08-16 10:41:43 +0200

    trying async/await commands in the create-react-app environment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -22,4 +23,35 @@ function App() {
   );
 }
 
+export default App; */
+import React, { Component } from "react"; 
+import ReactDOM from "react-dom"; 
+
+class App extends Component{
+  constructor(){
+    super(); 
+    this.state = { data: [] }; 
+  }
+
+  async componentDidMount(){
+    const response = await fetch ('https://api.coinmarketcap.com/v1/ticker/?limit=10'); 
+    const json = await response.json(); 
+    this.setState({ data: json})
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.data.map(el => (
+            <li>
+              {el.name}: {el.price_usd}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
 export default App;
+
